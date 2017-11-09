b739cbebf3bde49a9a63e662a46ce6b6a8a38156
mabr3 <miguel.b.rodrigues@tecnico.ulisboa.pt>
2017-11-09 11:25:23 +0000

    final com diagrama pipelina

--- PSD_P/P2/P2.sim/sim_1/impl/timing/top_circuit_tb_time_impl.v
+++ PSD_P/P2/P2.sim/sim_1/impl/timing/top_circuit_tb_time_impl.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2016.4 (lin64) Build 1756540 Mon Jan 23 19:11:19 MST 2017
-// Date        : Mon Nov  6 11:46:03 2017
+// Date        : Wed Nov  8 17:10:55 2017
 // Host        : M running 64-bit Ubuntu 16.04.3 LTS
 // Command     : write_verilog -mode timesim -nolib -sdf_anno true -force -file
 //               /home/miguel/Desktop/PSD_Proj/PSD_P/P2/P2.sim/sim_1/impl/timing/top_circuit_tb_time_impl.v
